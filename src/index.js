28e24f27db37392bec9d150ee76bad596e5b2ed5
Wayne Kiprotich <wanekip123@icloud.com>
2025-12-19 17:09:53 +0300

    Assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+    // Add event listener to form
+    form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Get the value
+    const taskInput = document.querySelector('input[name="new-task-description"]').value;
+    // Build the to-do
+    buildToDo(taskInput);
+    // To clear input
+    event.target.reset();
+  });
+  
+ //Function to build to-do
+  function buildToDo(task) {
+    const li = document.createElement('li');
+    li.textContent = task;
+    const taskList = document.getElementById('tasks');
+    taskList.appendChild(li);
+  }
 });
